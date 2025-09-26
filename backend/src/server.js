d30db11d679cf8ac21e423fa70eb99990d597999
Marlin Soe <marlin@Marlins-MacBook-Air.local>
2025-09-26 13:02:37 +0630

    prepare for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,10 +4,13 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv'
 import cors from 'cors'
 
+import path from 'path'
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 //middle ware
 app.use(express.json())
@@ -15,9 +18,17 @@ app.use(cors({origin: 'http://localhost:5173'}));
 
 app.use('/api/to-do-list', toDoRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
+
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Server is listening on ${PORT}.`);
     });
-})
+});
 
